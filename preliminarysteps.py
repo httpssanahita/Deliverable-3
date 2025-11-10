5f29268f644dd60df97da562396627b0516e99ab
jasmibri <6309052@edu.vaniercollege.qc.ca>
2025-11-10 15:15:45 -0500

    #2 done

---
+++ preliminarysteps.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data= pd.read_csv ("air_quality_health_dataset.csv")
+
+#a
+print(data.head(5)) 
+print(data.tail(5))
+print(data.shape)
+print(data.info())
+print(data.describe())
+
+#b
+print(data.duplicated().drop_duplicates())
+
+#c
+print(pd.isnull(data).sum()) #There aren't any null values
+
+#d
+a= pd.to_datetime(data["date"])
+
+print(a)
